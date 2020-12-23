8713c8fa56095b6903bb1082cde5c65106a27417
wicked-smart <marzaanvan@gmail.com>
2020-12-23 09:21:12 +0530

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,9 +10,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+         </Link>
+       </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -19,7 +23,7 @@ export default function Home() {
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
+            <h3>Procrastination &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
